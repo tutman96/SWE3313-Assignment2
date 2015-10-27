fd42d51aa0fb5e64b34f96491fc3b823ad59bd1e
Brandon Tuttle <brandon@Brandons-MacBook-Pro.local>
2015-10-27 11:16:59 -0400

    Electron init

---
+++ main.js
@@ -0,0 +1,26 @@
+///<reference path="typings/tsd.d.ts"/>
+
+var app = require('app');  // Module to control application life.
+var BrowserWindow = require('browser-window');  // Module to create native browser window.
+
+// Report crashes to our server.
+require('crash-reporter').start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    app.quit();
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function () {
+	mainWindow = new BrowserWindow({ width: 800, height: 600 });
+
+	mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+	mainWindow.webContents.openDevTools({ detach: true });
+});
