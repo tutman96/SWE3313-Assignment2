60a85f17499f96ee5b1c30a760f57bbe7198c938
Brandon Tuttle <brandon@Brandons-MacBook-Pro.local>
2015-10-27 11:27:21 -0400

    Added angular typings definitions

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,6 @@
 
 /// <reference path="github-electron/github-electron.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
