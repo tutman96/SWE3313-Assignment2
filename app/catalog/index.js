cf20c0c96d51c24dbbdcc402f11d62f6364d63e8
Brandon Tuttle <brandon@Brandons-Mac-Pro.local>
2015-11-17 16:00:32 -0500

    Prevent file drops in main window

--- app/catalog/index.js
+++ app/catalog/index.js
@@ -46,3 +46,11 @@ app.config(['$stateProvider', '$locationProvider',
             }
         });
     }]);
+document.addEventListener('dragover', function (event) {
+    event.preventDefault();
+    return false;
+}, false);
+document.addEventListener('drop', function (event) {
+    event.preventDefault();
+    return false;
+}, false);
